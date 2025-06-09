4003848c0294af52b037b52391c67f225d8903fb
Tobias Ottenweller <tobi@ottenweller.net>
2025-06-09 17:23:48 +0200

    feat: add flutter example

---
+++ flutter_example/lib/main.dart
@@ -0,0 +1,72 @@
+import 'package:big_decimal/big_decimal.dart';
+import 'package:big_decimal_intl/big_decimal_intl.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  BigDecimal _counter = BigDecimal.zero;
+  final formatter = BigDecimalFormatter(
+    NumberFormat.decimalPatternDigits(locale: 'en-US', decimalDigits: 1),
+  );
+
+  void _incrementCounter() {
+    setState(() {
+      _counter = _counter + BigDecimal.parse('0.5');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('You have pushed the button this many times:'),
+            Text(
+              formatter.format(_counter),
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
